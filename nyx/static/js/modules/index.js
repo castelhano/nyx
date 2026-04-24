eaf7a2ccbbad784ec4de1f73ef09974bb5929dde
Rafael Alves <castelhano.rafael@gmail.com>
2026-04-24 11:56:28 -0400

    ajustes

--- nyx/static/js/modules/index.js
+++ nyx/static/js/modules/index.js
@@ -1,15 +1,15 @@
-NyxModules["index"] = {
-    init(container) {
-        const group = container.dataset.keybindGroup;
+// NyxModules["index"] = {
+//     init(container) {
+//         const group = container.dataset.keybindGroup;
 
-        keys.bind('g+i', () => console.log('ACIONADO'), {
-            group,
-            element: document.getElementById('nome'),
-            desc:    'Teste de bind no index',
-            origin:  'nyx.index',
-        });
-    },
-    destroy() {
-        keys.unbindGroup(container.dataset.keybindGroup);
-    },
-};
+//         keys.bind('g+i', () => console.log('ACIONADO'), {
+//             group,
+//             element: document.getElementById('nome'),
+//             desc:    'Teste de bind no index',
+//             origin:  'nyx.index',
+//         });
+//     },
+//     destroy() {
+//         keys.unbindGroup(container.dataset.keybindGroup);
+//     },
+// };
