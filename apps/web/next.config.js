e389a04673ba370e6a5c35cd0d3f43d2adf17348
rafael <castelhano.rafael@gmail.com>
2026-05-14 19:58:51 -0400

    Ajustes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@nyx/schemas', '@nyx/types'],
+  allowedDevOrigins: ['192.168.15.13'],
 }
 
 module.exports = nextConfig
